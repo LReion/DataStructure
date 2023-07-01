64b55ac9c9c8df8ddbd8c457f21cc2509f3bea2a
Cienanos <cienanos1210@outlook.com>
2023-07-01 20:01:33 +0800

    Added basic operations for stacks

--- Stack.cpp
+++ Stack.cpp
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #define maxSize 20
 // 顺序栈定义
 typedef struct {
@@ -30,3 +31,75 @@ typedef struct {
   QNode *front; // 队头指针
   QNode *rear;  // 队尾指针
 } LiQueue;      // 链队类型定义
+
+// 初始化栈
+void initStack(SqStack &st) {
+  st.top = -1; // 这里规定将栈顶指针设置为-1时，栈为空
+}
+
+// 判断栈是否为空
+/* 栈st为空时返回1，否则返回0，其对应代码如下 */
+int isEmpty(SqStack st) {
+  if (st.top == -1)
+    return 1;
+  else
+    return 0;
+}
+
+// 进栈代码
+int push(SqStack &st, int x) {
+  if (st.top == maxSize - 1) // 栈满不能进栈
+    return 0;
+  st.data[++st.top] = x; // ++st.top先移动栈道顶指针，再进栈数据x
+  return 1;
+}
+
+// 出栈代码
+int pop(SqStack &st, int &x) {
+  if (st.top == -1) // 如果栈为空，则不能出栈
+    return 0;
+  x = st.data[st.top--]; // 先出栈，再移动栈顶指针
+  return 1;
+}
+
+// 链栈的基本操作
+// 初始化链栈
+void initStack(LNode *&lst) {
+  lst = (LNode *)malloc(sizeof(LNode)); // 分配一个头结点
+  lst->next = nullptr;                  // 头结点的指针域置空
+}
+
+// 判断栈空代码
+/* 当栈空时返回1，否则返回0，代码如下 */
+int isEmpty(LNode *lst) { // 判断是否为空
+  if (lst->next = nullptr)
+    return 1;
+  else
+    return 0;
+}
+
+// 进栈代码
+void push(LNode *lst, int x) {
+  LNode *p;
+  p = (LNode *)malloc(sizeof(LNode)); // 为进栈元素申请结点空间
+  p->next = nullptr; // 申请新节点时，将其指针域设置为nullptr可以避免一些错误
+
+  /*   以下三句就是链表的头插法 */
+  p->data = x;
+  p->next = lst->next;
+  lst->next = p;
+}
+
+// 出栈代码
+/* 在栈不空的情况下可以执行，返回1，否则返回0，代码如下： */
+int pop(LNode *lst, int &x) {
+  LNode *p;
+  if (lst->next == nullptr)
+    return 0;
+  // 单链表的删除操作
+  p = lst->next;
+  x = p->data;
+  lst->next = p->next;
+  free(p);
+  return 1;
+}
